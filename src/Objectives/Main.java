c31d2190209c056cbca403e8e99efb3292a8ae9d
vasujain275 <vasujain275@gmail.com>
2024-08-17 01:36:47 +0530

    feat: Redone the Objectives and Q1 of Practical 1

--- src/Objectives/Main.java
+++ src/Objectives/Main.java
@@ -1,71 +1,63 @@
 package Objectives;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
-
-    public static List<String> readFile(String filename) throws IOException {
-        // Read all lines and split each line into words
-        return Files.lines(Paths.get(filename))
-                .flatMap(line -> Stream.of(line.split("\\s+")))
-                .collect(Collectors.toList());
-    }
-
-    public static void displayWords(List<String> words, String description) {
-        System.out.println("\n" + description + ":");
-        System.out.println(words);
-        System.out.println("Number of words: " + words.size());
-    }
-
-    public static List<String> reverseWords(List<String> words) {
-        List<String> reversed = new ArrayList<>(words);
-        Collections.reverse(reversed);
-        return reversed;
-    }
-
-    public static List<String> getPlurals(List<String> words) {
-        return words.stream()
-                .filter(word -> word.endsWith("s"))
-                .collect(Collectors.toList());
-    }
-
-    public static List<String> removePlurals(List<String> words) {
-        return words.stream()
-                .filter(word -> !word.endsWith("s"))
-                .collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the filename: ");
-        String filename = scanner.nextLine();
-
+    public static void main(String[] args){
         try {
-            List<String> words = readFile(filename);
+            File myobj = new File("src/Objectives/test.txt");
+            Scanner sc = new Scanner(myobj);
+            ArrayList<String> list = new ArrayList<String>();
 
-            displayWords(words, "All words");
+            while(sc.hasNext()){
+                list.add(sc.next());
+            }
+            sc.close();
 
-            List<String> reversedWords = reverseWords(words);
-            displayWords(reversedWords, "Words in reverse order");
+            System.out.println("All the Words - \n");
+            list.forEach((word) -> System.out.print(word + " "));
 
-            List<String> plurals = getPlurals(words);
-            displayWords(plurals, "Plural words (ending with 's')");
+            System.out.println("All words in the Reverse Order - \n");
 
-            List<String> nonPlurals = removePlurals(words);
-            displayWords(nonPlurals, "Words with plurals removed");
+            ArrayList<String> revList = new ArrayList<String>();
 
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
-        } finally {
-            scanner.close();
+            for (int i = list.size() - 1; i >= 0; i--) {
+                revList.add(list.get(i));
+            }
+
+            revList.forEach((word) -> System.out.print(word + " "));
+
+            System.out.println("\n All the plurals - \n");
+
+            for (String s : list) {
+                if (s.endsWith("s")){
+                    System.out.print(s + " ");
+                }
+            }
+
+            System.out.println("\n With All plurals removed \n");
+
+            for (int i = 0; i < list.size(); i++) {
+
+                String word =  list.get(i);
+
+                if(word.endsWith("s")){
+                    list.remove(i);
+                }
+            }
+
+
+            list.forEach((word) -> System.out.print(word + " "));
+
+            System.out.println("\n Size of new list - " + list.size());
+
+
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found error");
+            e.printStackTrace();
         }
     }
 }
--- src/Practical01/Question01/Main.java
+++ src/Practical01/Question01/Main.java
@@ -1,5 +1,5 @@
 package Practical01.Question01;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -10,35 +10,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the file path: ");
-        String filePath = scanner.nextLine();
-
-        System.out.print("Enter the number of lines to print: ");
-        int numberOfLines = scanner.nextInt();
-        scanner.close();
-
         try {
-            List<String> lines = Files.readAllLines(Paths.get(filePath));
-            List<String> randomLines = getRandomLines(lines, numberOfLines);
-            for (String line : randomLines) {
-                System.out.println(line);
+            BufferedReader reader;
+            reader = new BufferedReader(new FileReader("src/Practical01/Question01/test.txt"));
+            int lines = 0;
+            while (reader.readLine() != null) lines++;
+            List<String> linesList = new ArrayList<String>(lines);
+            reader.close();
+
+            File myfile = new File("src/Practical01/Question01/test.txt");
+            Scanner sc = new Scanner(myfile);
+
+            while (sc.hasNextLine()){
+                linesList.add(sc.nextLine());
             }
+
+            printRandomLines(linesList, 3);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
-        }
-    }
-
-    private static List<String> getRandomLines(List<String> lines, int numberOfLines) {
-        List<String> randomLines = new ArrayList<>(numberOfLines);
-        Random random = new Random();
-
-        for (int i = 0; i < numberOfLines; i++) {
-            int randomIndex = random.nextInt(lines.size());
-            randomLines.add(lines.get(randomIndex));
+            throw new RuntimeException(e);
         }
 
-        return randomLines;
     }
+
+    public static void printRandomLines(List<String> list, int count) {
+        Random rand = new Random();
+
+        int max = list.size();
+
+        for (int i = 0; i < count; i++) {
+            int randomNumber = rand.nextInt(max - 1) + 1;
+            String line = list.get(randomNumber);
+            System.out.println(line);
+        }
+
+    }
+
 }
